a3c339eeb024544a4bd249043cf03f24c8ad6725
codingWithSharon <sharon_dt@hotmail.com>
2025-11-20 20:20:44 +0100

    Modified first test

--- 001_AT_How_to_set_up_a_test_automation_project/UITests/PageObjectModels/UITestingPlaygroundPages/UITestingPlaygroundPage.cs
+++ 001_AT_How_to_set_up_a_test_automation_project/UITests/PageObjectModels/UITestingPlaygroundPages/UITestingPlaygroundPage.cs
@@ -4,20 +4,27 @@ namespace _001_AT_How_to_set_up_a_test_automation_project.UITests.PageObjectMode
 
 public class UITestingPlaygroundPage : BasePage
 {
-    #region general
+    
     public UITestingPlaygroundPage(IPage page) : base(page) { }
-    public ILocator _homePageTitle => Page.Locator("#title");
+
+    #region nav bar
     public ILocator _navBarExpandButton => Page.Locator("xpath=//button[@class='navbar-toggler collapsed']");
     public ILocator _navBarCollapseButton => Page.Locator("xpath=//button[@class='navbar-toggler']");
-    public ILocator _homePageOverview => Page.Locator("#overview");
-    public ILocator SelectAutomationPitfalls(int row, int column) { return Page.Locator($"xpath=/html/body/section[2]/div/div[{row}]/div[{column}]/h3/a"); }
-
+    public ILocator _navBarHomeButton => Page.Locator("xpath=(//*[@class='nav-link'])[1]");
+    public ILocator _navBarResourcesButton => Page.Locator("xpath=(//*[@class='nav-link'])[2]");
     #endregion
 
+    #region overview homepage
+    public ILocator _homePageTitle => Page.Locator("#title");
+    public ILocator _homePageOverview => Page.Locator("#overview");
+    public ILocator SelectAutomationPitfalls(int row, int column) { return Page.Locator($"xpath=/html/body/section[2]/div/div[{row}]/div[{column}]/h3/a"); }
+    #endregion
 
+    #region dynamic page
+    public ILocator _dynamicPageDynamicButton => Page.Locator("xpath=//*[@class='btn btn-primary']");
+    #endregion
 
     #region basic operations
-
     public async Task GoToUITestingPlayground()
     {
         await Page.GotoAsync("http://www.uitestingplayground.com/");
@@ -33,10 +40,19 @@ public class UITestingPlaygroundPage : BasePage
         await _navBarCollapseButton.ClickAsync();
     }
 
+    public async Task ClickHomeButton()
+    {
+        await _navBarHomeButton.ClickAsync();
+    }
+
+    public async Task ClickResourcesButton()
+    {
+        await _navBarResourcesButton.ClickAsync();
+    }
+
     public async Task SelectAnAutomationPitfall(int row, int column)
     {
         await SelectAutomationPitfalls(row, column).ClickAsync();
     }
-
     #endregion
 }
--- 001_AT_How_to_set_up_a_test_automation_project/UITests/TestFixtures/UITestingPlaygroundTests/UITestingPlaygroundTests.cs
+++ 001_AT_How_to_set_up_a_test_automation_project/UITests/TestFixtures/UITestingPlaygroundTests/UITestingPlaygroundTests.cs
@@ -9,11 +9,17 @@ namespace _001_AT_How_to_set_up_a_test_automation_project.UITests.Testfixtures;
 public class UITestingPlaygroundTests : Setup
 {
     [Test, Retry(2)]
-    public async Task NavigatingTheHomePAge()
+    public async Task NavigatingTheWebSite()
     {
         await uITestingPlaygroundPage.GoToUITestingPlayground();
         await Expect(uITestingPlaygroundPage._homePageTitle).ToBeVisibleAsync();
         await Expect(uITestingPlaygroundPage._homePageOverview).ToBeVisibleAsync();
         await uITestingPlaygroundPage.SelectAnAutomationPitfall(1, 1);
+        await Expect(uITestingPlaygroundPage._dynamicPageDynamicButton).ToContainTextAsync("Dynamic ID");
+        await Expect(uITestingPlaygroundPage._homePageOverview).Not.ToBeVisibleAsync();
+        await uITestingPlaygroundPage.ClickHomeButton();
+        await Expect(uITestingPlaygroundPage._dynamicPageDynamicButton).Not.ToBeVisibleAsync();
+        await Expect(uITestingPlaygroundPage._homePageOverview).ToBeVisibleAsync();
+        Console.WriteLine("Quick navigation of the website to check that basic functionality is working");
     }
 }
